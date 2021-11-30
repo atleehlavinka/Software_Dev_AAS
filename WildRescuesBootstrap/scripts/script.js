2d970e5097e0676fd29e1e219840afe043894b8a
atleehlavinka <81734135+atleehlavinka@users.noreply.github.com>
2021-11-29 23:06:02 -0700

    Updated folder system

--- WildRescuesBootstrap/scripts/script.js
+++
@@ -1,9 +0,0 @@
-/*
-    Student Name: Atlee Hlavinka
-    File Name: script.js
-    Date: 04/28/2021
-*/
-//JQuery for hero image to consume the header window space
-    $(document).ready(function(){
-        $('.hero').height($(window).height());
-    });
