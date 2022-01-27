7b32e9c83e577ce9f74415648d58ff22199427ba
atleehlavinka <81734135+atleehlavinka@users.noreply.github.com>
2022-01-26 18:36:39 -0700

    Add files via upload

---
+++ SWDV-210 - Intro to Server Side Programming/Book Examples/Chapter 2/display_discount.php
@@ -0,0 +1,36 @@
+<?php
+    $product_description = $_POST['product_description'];
+    $list_price = $_POST['list_price'];
+    $discount_percent = $_POST['discount_percent'];
+    
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Product Discount Calculator</title>
+    <link rel="stylesheet" type="text/css" href="main.css" />
+</head>
+<body>
+    <div id="content">
+        <h1>This page is under construction</h1>
+
+        <label>Product Description:</label>
+        <span><?php echo $product_description; ?></span><br />
+
+        <label>List Price:</label>
+        <span><?php echo $list_price_formatted; ?></span><br />
+
+        <label>Standard Discount:</label>
+        <span><?php echo $discount_percent_formatted; ?></span><br />
+
+        <label>Discount Amount:</label>
+        <span><?php echo $discount_formatted; ?></span><br />
+
+        <label>Discount Price:</label>
+        <span><?php echo $discount_price_formatted; ?></span><br />
+
+        <p>&nbsp;</p>
+    </div>
+</body>
+</html>
