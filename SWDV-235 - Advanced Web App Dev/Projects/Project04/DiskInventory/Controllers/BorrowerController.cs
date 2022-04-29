2869497cfe830184d9d13f1d5bed08ba82a04617
atleehlavinka <atleehlavinka@outlook.com>
2022-04-29 15:58:42 -0600

    Added functionality for checkout

---
+++ SWDV-235 - Advanced Web App Dev/Projects/Project04/DiskInventory/Controllers/BorrowerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DiskInventory.Models;
+
+namespace DiskInventory.Controllers
+{
+    
+    public class BorrowerController : Controller
+    {
+        private disk_inventoryahContext context { get; set; }
+        public BorrowerController(disk_inventoryahContext ctx)
+        {
+            context = ctx;
+        }
+        public IActionResult Index()
+        {
+            List<DiskBorrower> borrowers = context.DiskBorrowers.OrderBy(b => b.Lname).ThenBy(b => b.Fname).ToList();
+            return View(borrowers);
+        }
+    }
+}
---
+++ SWDV-235 - Advanced Web App Dev/Projects/Project04/DiskInventory/Models/DiskHasBorrowerViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiskInventory.Models
+{
+    public partial class DiskHasBorrowerViewModel
+    {
+        public int DiskHasBorrowerId { get; set; }
+        public int BorrowerId { get; set; }
+        public int DiskId { get; set; }
+        public DateTime BorrowedDate { get; set; }
+        public DateTime? ReturnedDate { get; set; }
+
+        public virtual DiskBorrower Borrower { get; set; }
+        public virtual Disk Disk { get; set; }
+        public List<DiskBorrower> Borrowers { get; set; }
+        public List<Disk> Disks { get; set; }
+    }
+}
